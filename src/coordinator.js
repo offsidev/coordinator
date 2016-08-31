54f3e7aa9977f84d3a70b81610353577d22bfcd9
wissam <offsidev@gmail.com>
2016-08-31 13:04:29 -0700

    Another attempt at resolving indentation issue.

--- src/coordinator.js
+++ src/coordinator.js
@@ -1,165 +1,165 @@
 (function (root, factory) {
 
-	if ( typeof exports === 'object' && typeof module === 'object' )
-		module.exports = factory();
+   if ( typeof exports === 'object' && typeof module === 'object' )
+      module.exports = factory();
 
-	else if ( typeof define === 'function' && define.amd )
-		define('Coordinator', [], factory);
+   else if ( typeof define === 'function' && define.amd )
+      define('Coordinator', [], factory);
 
-	else if ( typeof exports === 'object' )
-		exports['Coordinator'] = factory();
+   else if ( typeof exports === 'object' )
+      exports['Coordinator'] = factory();
 
-	else
-		root['Coordinator'] = factory();
+   else
+      root['Coordinator'] = factory();
 
 })(this, function () {
 
-	'use strict';
+   'use strict';
 
-	/**
-	 * Event matrix contains the registered events and subscribed listeners
-	 *
-	 * @private
-	 * @type {Object}
-	 */
-	var eventCenter = {};
-
-	/**
-	 * Returns the subscriber list for the passed-in event
+   /**
+    * Event matrix contains the registered events and subscribed listeners
     *
-    * @param {String} ev is the event name
-    * @return {Array} list of subscribers
+    * @private
+    * @type {Object}
     */
-	var _getSubscribers = function (ev) {
-		return eventCenter[ev] || [];
-	};
+   var eventCenter = {};
 
-	/**
-  	 * Subscribes a list of listeners to an event
-  	 *
-  	 * @param {String} ev is the event name
-  	 * @param {Array} subscribers
-  	 * @return {Array} list of subscribers
-  	 */
-	var _setSubscribers = function (ev, subscribers) {
-		if ( !subscribers instanceof Array )
-         return false;
-		eventCenter[ev] = subscribers;
-	};
+/**
+* Returns the subscriber list for the passed-in event
+*
+* @param {String} ev is the event name
+* @return {Array} list of subscribers
+*/
+var _getSubscribers = function (ev) {
+return eventCenter[ev] || [];
+};
 
-	/**
-  	 * Checks whether the passed-in event is registered
-  	 *
-  	 * @param {String} ev is the event name
-  	 * @return {Boolean}
-  	 */
-	var _isEventRegistered = function (ev) {
-      return ( typeof eventCenter[ev] != 'undefined' ) ? true : false;
-	};
+/**
+* Subscribes a list of listeners to an event
+*
+* @param {String} ev is the event name
+* @param {Array} subscribers
+* @return {Array} list of subscribers
+*/
+var _setSubscribers = function (ev, subscribers) {
+if ( !subscribers instanceof Array )
+return false;
+eventCenter[ev] = subscribers;
+};
 
-	/**
-	 * Removes the passed-in event from the event matrix
-	 *
-	 * @param {String} ev is the event name
-	 */
-	var _deRegisterEvent = function (ev) {
-      if ( _isEventRegistered(ev) )
-         delete eventCenter[ev];
-	};
+/**
+* Checks whether the passed-in event is registered
+*
+* @param {String} ev is the event name
+* @return {Boolean}
+*/
+var _isEventRegistered = function (ev) {
+return ( typeof eventCenter[ev] != 'undefined' ) ? true : false;
+};
 
-	/**
-	 * Subscribes the passed-in function to the passed-in event
-	 *
-	 * @param {String} ev is the event name
-	 * @param {Function} fn listener function
-	 * @param {(Object|null)} scope is the scope context of the listener function
-	 * @return {(Boolean|Number)}
-	 */
-	var subscribe = function (ev, fn, scope) {
-		var numberOfArguments = arguments.length;	
-		if ( numberOfArguments < 2 )
-			return false;	
-		else {
-			if ( typeof ev != 'string' || typeof fn != 'function' )
-				return false;
-			if ( typeof scope == 'undefined' )
-				scope = null;
-		}
+/**
+* Removes the passed-in event from the event matrix
+*
+* @param {String} ev is the event name
+*/
+var _deRegisterEvent = function (ev) {
+if ( _isEventRegistered(ev) )
+delete eventCenter[ev];
+};
 
-		if ( !_isEventRegistered(ev) )
-			eventCenter[ev] = [];
+/**
+* Subscribes the passed-in function to the passed-in event
+*
+* @param {String} ev is the event name
+* @param {Function} fn listener function
+* @param {(Object|null)} scope is the scope context of the listener function
+* @return {(Boolean|Number)}
+*/
+var subscribe = function (ev, fn, scope) {
+var numberOfArguments = arguments.length;   
+if ( numberOfArguments < 2 )
+return false;   
+else {
+if ( typeof ev != 'string' || typeof fn != 'function' )
+return false;
+if ( typeof scope == 'undefined' )
+scope = null;
+}
 
-		return eventCenter[ev].push({
-			fn: fn,
-			scp: scope
-		});
-	};
+if ( !_isEventRegistered(ev) )
+eventCenter[ev] = [];
 
-	/**
-	 * Broadcast supplied event to its subscribers with optional data
-    *
-    * @param {String} ev is the event name
-    * @param {*} data
-    * @return {Boolean}
-    */
-	var broadcast = function (ev, data) {
-		if ( !_isEventRegistered(ev) )
-			return false;
+return eventCenter[ev].push({
+fn: fn,
+scp: scope
+});
+};
 
-		var subscribers = _getSubscribers(ev);
-		for ( var i = 0; i < subscribers.length; i++ ) {
-			if ( typeof data == 'undefined' )
-				subscribers[i]['fn'].apply(subscribers[i]['scp']);
-			else
-				subscribers[i]['fn'].apply(subscribers[i]['scp'], [data]);
-		}
-		return true;
-	};
+/**
+* Broadcast supplied event to its subscribers with optional data
+*
+* @param {String} ev is the event name
+* @param {*} data
+* @return {Boolean}
+*/
+var broadcast = function (ev, data) {
+if ( !_isEventRegistered(ev) )
+return false;
 
-	/**
-	 * Unsubscribes the passed-in function from the passed-in event's list
-    *
-    * @param {String} ev is the event name
-    * @param {Function} fn listener function
-    * @param {(Object|null)} scope is the scope context of the listener function
-    * @return {Boolean}
-    */
-	var unsubscribe = function (ev, fn, scope) {
-		var numberOfArguments = arguments.length;		
-		if ( numberOfArguments < 2 )
-			return false;		
-		else {
-			if ( typeof ev != 'string' || typeof fn != 'function' )
-				return false;
-			if ( typeof scope == 'undefined' )
-				scope = null;
-		}
+var subscribers = _getSubscribers(ev);
+for ( var i = 0; i < subscribers.length; i++ ) {
+if ( typeof data == 'undefined' )
+subscribers[i]['fn'].apply(subscribers[i]['scp']);
+else
+subscribers[i]['fn'].apply(subscribers[i]['scp'], [data]);
+}
+return true;
+};
 
-		if ( _isEventRegistered(ev) ) {
-			var updatedSubscriberList = [],
-			   subscriberList = _getSubscribers(ev),
-				subscriber;
+/**
+* Unsubscribes the passed-in function from the passed-in event's list
+*
+* @param {String} ev is the event name
+* @param {Function} fn listener function
+* @param {(Object|null)} scope is the scope context of the listener function
+* @return {Boolean}
+*/
+var unsubscribe = function (ev, fn, scope) {
+var numberOfArguments = arguments.length;       
+if ( numberOfArguments < 2 )
+return false;       
+else {
+if ( typeof ev != 'string' || typeof fn != 'function' )
+return false;
+if ( typeof scope == 'undefined' )
+scope = null;
+}
 
-			for (var i = 0; i < subscriberList.length; i++) {
-				subscriber = subscriberList[i];
-				if ( subscriber['fn'] != fn || subscriber['scp'] != scope )
-					updatedSubscriberList.push(subscriber);
-			};
+if ( _isEventRegistered(ev) ) {
+var updatedSubscriberList = [],
+subscriberList = _getSubscribers(ev),
+subscriber;
 
-			_setSubscribers(ev, updatedSubscriberList);
-		}
+for (var i = 0; i < subscriberList.length; i++) {
+subscriber = subscriberList[i];
+if ( subscriber['fn'] != fn || subscriber['scp'] != scope )
+updatedSubscriberList.push(subscriber);
+};
 
-		return true;
-	};
+_setSubscribers(ev, updatedSubscriberList);
+}
 
-	return {
-		subscribe: subscribe,
-		unsubscribe: unsubscribe,
-		broadcast: broadcast,
-		_getSubscribers: _getSubscribers,
-		_setSubscribers: _setSubscribers,
-		_deRegisterEvent: _deRegisterEvent,
-	};
+return true;
+};
+
+return {
+subscribe: subscribe,
+unsubscribe: unsubscribe,
+broadcast: broadcast,
+_getSubscribers: _getSubscribers,
+_setSubscribers: _setSubscribers,
+_deRegisterEvent: _deRegisterEvent,
+};
 
 });
 
