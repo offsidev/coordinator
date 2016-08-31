0a8916cc59b2db991421712d6f4d8b63ac4406cd
wissam <offsidev@gmail.com>
2016-08-30 23:34:33 -0700

    Wrote docblocks for module properties and methods.

--- src/coordinator.js
+++ src/coordinator.js
@@ -16,27 +16,65 @@
 
 	'use strict';
 
+	/**
+	  * Event matrix contains the registered events and subscribed listeners
+      *
+      * @private
+      * @type {Object}
+      */
 	var eventCenter = {};
 
+	/**
+	  * Returns the subscriber list for the passed-in event
+      *
+      * @param {String} ev is the event name
+      * @return {Array} list of subscribers
+      */
 	var _getSubscribers = function (ev) {
 		return eventCenter[ev] || [];
 	};
 
+	/**
+	  * Subscribes a list of listeners to an event
+      *
+      * @param {String} ev is the event name
+      * @param {Array} subscribers
+      * @return {Array} list of subscribers
+      */
 	var _setSubscribers = function (ev, subscribers) {
 		if ( !subscribers instanceof Array )
 			return false;
 		eventCenter[ev] = subscribers;
 	};
 
+	/**
+	  * Checks whether the passed-in event is registered
+      *
+      * @param {String} ev is the event name
+      * @return {Boolean}
+      */
 	var _isEventRegistered = function (ev) {
 		return ( typeof eventCenter[ev] != 'undefined' ) ? true : false;
 	};
 
+	/**
+	  * Removes the passed-in event from the event matrix
+      *
+      * @param {String} ev is the event name
+      */
 	var _deRegisterEvent = function (ev) {
 		if ( _isEventRegistered(ev) )
 			delete eventCenter[ev];
 	};
 
+	/**
+	  * Subscribes the passed-in function to the passed-in event
+      *
+      * @param {String} ev is the event name
+      * @param {Function} fn listener function
+      * @param {(Object|null)} scope is the scope context of the listener function
+      * @return {(Boolean|Number)}
+      */
 	var subscribe = function (ev, fn, scope) {
 		var numberOfArguments = arguments.length;	
 		if ( numberOfArguments < 2 )
@@ -57,6 +95,13 @@
 		});
 	};
 
+	/**
+	  * Broadcast supplied event to its subscribers with optional data
+      *
+      * @param {String} ev is the event name
+      * @param {*} data
+      * @return {Boolean}
+      */
 	var broadcast = function (ev, data) {
 		if ( !_isEventRegistered(ev) )
 			return false;
@@ -71,6 +116,14 @@
 		return true;
 	};
 
+	/**
+	  * Unsubscribes the passed-in function from the passed-in event's list
+      *
+      * @param {String} ev is the event name
+      * @param {Function} fn listener function
+      * @param {(Object|null)} scope is the scope context of the listener function
+      * @return {Boolean}
+      */
 	var unsubscribe = function (ev, fn, scope) {
 		var numberOfArguments = arguments.length;		
 		if ( numberOfArguments < 2 )
